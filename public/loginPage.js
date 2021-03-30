d13235424967fd2017b667ad76e1e5ed9587e5d8
Nikita Efremov <nikita-efremov-00@mail.ru>
2021-03-30 11:23:45 +0400

    first commit delete throw

--- public/loginPage.js
+++ public/loginPage.js
@@ -5,7 +5,7 @@ const userForm = new UserForm();
 userForm.loginFormCallback = data => {
     ApiConnector.login(data, response => {
         if (!response.success) {
-            throw userForm.setLoginErrorMessage(response.error);
+            userForm.setLoginErrorMessage(response.error);
         } else {
             location.reload();
         }
@@ -16,7 +16,7 @@ userForm.registerFormCallback = data => {
     ApiConnector.register(data, response => {
         console.log(response);
         if (!response.success) {
-            throw userForm.setRegisterErrorMessage(response.error);
+            userForm.setRegisterErrorMessage(response.error);
         } else {
             location.reload();
         }
